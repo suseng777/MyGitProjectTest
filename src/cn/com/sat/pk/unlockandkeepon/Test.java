cff975e78aa54106c17c285dbc123d5ea51f9f9b
suseng777 <suseng777@163.com>
2015-09-22 14:11:19 +0800

    增加Log打印

--- src/cn/com/sat/pk/unlockandkeepon/Test.java
+++ src/cn/com/sat/pk/unlockandkeepon/Test.java
@@ -3,13 +3,15 @@ package cn.com.sat.pk.unlockandkeepon;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class Test extends BroadcastReceiver{
 
+	private static final String TAG_STRING = "TEST_SUKAISHENG";
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		// TODO Auto-generated method stub
-		
+		Log.e(TAG_STRING, "I got a Message!!!");
 	}
 
 }
